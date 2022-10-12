8a62cb2d3366e4f13d3cd64df8f7dedf6710386a
KseniaRumyantseva <92818237+KseniaRumyantseva@users.noreply.github.com>
2022-10-12 14:43:53 +0500

    update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-my_token = "5772983639:AAEUxrarVynmaYHy8otgugjsSP1Xsd9-cng"
-api_news = "233591469d874f86ad39b2246747f420"
+my_token = "Вставьте токен своего бота"
+api_news = "Вставьте api"
 
 categories = ["business", "entertainment", "health", "science", "sports", "technology"]
